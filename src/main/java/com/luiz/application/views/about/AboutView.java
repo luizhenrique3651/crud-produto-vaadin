833bf6b6aef7fd26fd1b919d656dd9648f2e70cb
luiz.coelho <uizhenrique@gmail.com>
2024-12-22 15:12:48 -0400

    Alterada pagina de about

--- src/main/java/com/luiz/application/views/about/AboutView.java
+++ src/main/java/com/luiz/application/views/about/AboutView.java
@@ -1,5 +1,8 @@
 package com.luiz.application.views.about;
 
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -8,7 +11,6 @@ import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
-import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
 @PageTitle("About")
 @Route("about")
@@ -18,14 +20,19 @@ public class AboutView extends VerticalLayout {
     public AboutView() {
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        Image img = new Image("images/perfil-arredondado.png", "Por hoje é só");
         img.setWidth("200px");
         add(img);
 
-        H2 header = new H2("This place intentionally left empty");
+        H2 header = new H2("Por hoje é só :)");
         header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
         add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        Paragraph paragraph = new Paragraph("Em breve subindo novos updates, me acompanhe no ");
+        Anchor linkedinLink = new Anchor("https://www.linkedin.com/in/luiz-henrique-coelho/", "Meu LinkedIn");
+        linkedinLink.setTarget("_blank"); // Abre o link em uma nova aba
+
+        paragraph.add(linkedinLink);
+        add(paragraph);
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
