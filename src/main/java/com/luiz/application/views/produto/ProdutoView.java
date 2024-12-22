19861229ade817833aa83acabc1f6fb91c500bc5
luiz.coelho <uizhenrique@gmail.com>
2024-12-22 14:45:01 -0400

    Atualizada Pagina de Produtos, trazendo o grid de produtos

--- src/main/java/com/luiz/application/views/produto/ProdutoView.java
+++ src/main/java/com/luiz/application/views/produto/ProdutoView.java
@@ -1,35 +1,82 @@
 package com.luiz.application.views.produto;
 
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.firitin.components.grid.VGrid;
+import org.vaadin.firitin.components.html.VDiv;
+import org.vaadin.firitin.components.orderedlayout.VVerticalLayout;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+import com.luiz.application.components.CustomButton;
+import com.luiz.application.components.CustomFormLayout;
+import com.luiz.application.data.entity.Produto;
+import com.luiz.application.data.service.ProdutoService;
+import com.luiz.application.util.CustomUtils;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
 @PageTitle("Produto")
 @Route("")
 @Menu(order = 0, icon = LineAwesomeIconUrl.PRODUCT_HUNT)
-public class ProdutoView extends HorizontalLayout {
+public class ProdutoView extends  VVerticalLayout implements BeforeEnterObserver  {
 
-    private TextField name;
-    private Button sayHello;
+	  private static final long serialVersionUID = 1L;
 
-    public ProdutoView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+		private final VGrid<Produto> gridProdutos;
+	    
+	    @Autowired
+	    private ProdutoService service;
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+	    public ProdutoView(final ProdutoService produtoService) {
+	        this.withFullWidth();
+	        this.withPadding(false);
 
-        add(name, sayHello);
-    }
+	        gridProdutos = new VGrid<>();
+	        gridProdutos.withThemeVariants(GridVariant.LUMO_NO_BORDER);
+	        gridProdutos.addColumn(Produto::getId).setHeader("ID");
+	        gridProdutos.addColumn(Produto::getDescricao).setHeader("Descrição");
+	        gridProdutos.addColumn(produto -> {
+			    return CustomUtils.formataValor(produto.getValorUnitario());
+	        }).setHeader("Valor");
+
+
+	        final var btnAdicionar = new CustomButton("Adicionar").themeTertiaryInline().themeSmall().withClassName("grid-actions")
+	            .withClickListener(ev -> new ProdutoForm(new Produto(), produtoService, consumerProduto -> {
+	            	produtoService.save(consumerProduto);
+	            	gridProdutos.setItems(produtoService.findProdutos());
+	            }).open());
+
+	        gridProdutos.addColumn(new ComponentRenderer<>(produto -> {
+	            final var btnEditar = new CustomButton("Editar").themeTertiaryInline().themeSmall()
+	                .withClickListener(ev -> new ProdutoForm(produto, produtoService, consumerProduto -> {
+	                	produto.setDescricao(consumerProduto.getDescricao());
+	                	produto.setValorUnitario(consumerProduto.getValorUnitario());
+	                	produtoService.save(produto);
+	                	gridProdutos.setItems(produtoService.findProdutos());
+	                }).open());
+
+	            final var btnRemover = new CustomButton("Excluir").themeTertiaryInline().themeError().themeSmall()
+	                .withClickListener(ev -> {
+	                	produtoService.deleteById(produto.getId());
+	                	gridProdutos.setItems(produtoService.findProdutos());
+	                });
+
+	            return new VDiv(btnEditar, btnRemover).withClassName("grid-actions");
+	        })).setHeader(btnAdicionar);
+
+	        final var options = new CustomFormLayout().withFullWidth();
+
+	        this.add(options, gridProdutos);
+	    }
+
+	    @Override
+	    public void beforeEnter(final BeforeEnterEvent beforeEnterEvent) {
+	        gridProdutos.setItems(
+	        		service.findProdutos());
+	    }
 
 }
